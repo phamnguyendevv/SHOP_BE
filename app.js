900ca6a64898df3796dfe8e104ab52f27e98c1b4
Pham Trung Nguyen <phamtrungnguyen2288@gmail.com>
2024-04-13 04:54:41 +0700

    update: cicd

--- app.js
+++ app.js
@@ -4,17 +4,15 @@ import indexRouter from './src/routers/index.js';
 import defaultErrorHandler from './src/middlewares/errorMiddewares.js'
 import dotenv from 'dotenv';
 dotenv.config();
-
+console.log('Server is starting...1')
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 
-
-
 app.use('/', indexRouter);
 
-
+console.log('Server is starting...2')
 //handle error
 
 app.use(defaultErrorHandler)
