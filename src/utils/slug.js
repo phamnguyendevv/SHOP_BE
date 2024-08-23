fbd73d592ddc67b360e3e623b9e5e928ceafea62
Nguyen <phamtrungnguyen2288@gmail.com>
2024-08-24 02:14:08 +0700

    update:

--- src/utils/slug.js
+++ src/utils/slug.js
@@ -1,29 +1,29 @@
-const getSlug = require('slug');
-const ProductModel = require('./ProductModel'); // Import model để tìm sản phẩm bằng slug
+const getSlug = require("slug");
+const ProductModel = require("./ProductModel"); // Import model để tìm sản phẩm bằng slug
 
 const createSlug = async (name, type) => {
-    const slug = getSlug(name, { lang: 'vn' });
-    let prefix;
-    if (type === 'cat') {
-        prefix = 'cat';
-    } else if (type === 'pro') {
-        prefix = 'pro';
-    } else {
-        throw new Error('Invalid slug type');
-    }
+  const slug = getSlug(name, { lang: "vn" });
+  let prefix;
+  if (type === "cat") {
+    prefix = "cat";
+  } else if (type === "pro") {
+    prefix = "pro";
+  } else {
+    throw new Error("Invalid slug type!");
+  }
 
-    let fullSlug, existingProduct;
-    do {
-        const randomInt = crypto.randomBytes(4).readUInt32LE();
-        fullSlug = `${slug}.${prefix}-${randomInt}`;
-        existingProduct = await ProductModel.getProductByField(
-            connection,
-            'slug',
-          fullSlug
-        );
-    } while (existingProduct);
+  let fullSlug, existingProduct;
+  do {
+    const randomInt = crypto.randomBytes(4).readUInt32LE();
+    fullSlug = `${slug}.${prefix}-${randomInt}`;
+    existingProduct = await ProductModel.getProductByField(
+      connection,
+      "slug",
+      fullSlug
+    );
+  } while (existingProduct);
 
-    return fullSlug;
+  return fullSlug;
 };
 
 module.exports = createSlug;
